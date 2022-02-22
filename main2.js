360c6cdadb1b0fe754e4b6fff42b9a2fe6b8b6f8
kevinderylo <76544058+kevinderylo@users.noreply.github.com>
2022-02-22 17:22:02 -0500

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,12 @@
+function room1(){
+    window.location="indexroom2.html";
+}
+function room2(){
+    window.location="indexroom3.html";
+}
+function room3(){
+    window.location="indexroom4.html";
+}
+function room4(){
+    window.location="indexroom5.html";
+}
